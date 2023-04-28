e9dd7e4fb58dc3629eea518ed17fade40f798216
pubudunav <45747365+pubudunav@users.noreply.github.com>
2023-04-28 19:14:55 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'other-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
